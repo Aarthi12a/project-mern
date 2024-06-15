cdce554d1de3cf38af550a02175e89a2efc8a245
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2024-06-16 04:04:49 +0500

    protected routes for better safety

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import SignUp from './pages/SignUp/SignUp'
+import ProtectedRoute from './ProtectedRoute'
 
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/dashboard" element={<Home/>} />
+          <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
+          <Route path="/dashboard" element={<ProtectedRoute><Home/></ProtectedRoute>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<SignUp/>} />
         </Routes>
